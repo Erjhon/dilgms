43f2646e862448bfa9aa853d0470c20d145de3e8
Erjhon Baldoza <erjhonbaldoza@gmail.com>
2023-05-04 11:03:40 +0800

    push

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -437,11 +437,11 @@
                   <div class="weather-info">
                     <div class="d-flex">
                       <div>
-                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
+                        <!-- <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2> -->
                       </div>
                       <div class="ml-2">
-                        <h4 class="location font-weight-normal">Bangalore</h4>
-                        <h6 class="font-weight-normal">India</h6>
+                        <h4 class="location font-weight-normal">Memorandum</h4>
+                        <h6 class="font-weight-normal">Notice!</h6>
                       </div>
                     </div>
                   </div>
@@ -453,18 +453,18 @@
                 <div class="col-md-6 mb-4 stretch-card transparent">
                   <div class="card card-tale">
                     <div class="card-body">
-                      <p class="mb-4">Today’s Bookings</p>
-                      <p class="fs-30 mb-2">4006</p>
-                      <p>10.00% (30 days)</p>
+                      <p class="mb-4">May Task</p>
+                      <!-- <p class="fs-30 mb-2">4006</p> -->
+                      <p class="text-info">Completed</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-md-6 mb-4 stretch-card transparent">
                   <div class="card card-dark-blue">
                     <div class="card-body">
-                      <p class="mb-4">Total Bookings</p>
-                      <p class="fs-30 mb-2">61344</p>
-                      <p>22.00% (30 days)</p>
+                      <p class="mb-4">June Task</p>
+                      <!-- <p class="fs-30 mb-2">61344</p> -->
+                      <p class="text-danger">Not Completed</p>
                     </div>
                   </div>
                 </div>
@@ -473,18 +473,18 @@
                 <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
                   <div class="card card-light-blue">
                     <div class="card-body">
-                      <p class="mb-4">Number of Meetings</p>
-                      <p class="fs-30 mb-2">34040</p>
-                      <p>2.00% (30 days)</p>
+                      <p class="mb-4">July Task</p>
+                      <!-- <p class="fs-30 mb-2">34040</p> -->
+                      <p class="text-danger">Not Completed</p>
                     </div>
                   </div>
                 </div>
                 <div class="col-md-6 stretch-card transparent">
                   <div class="card card-light-danger">
                     <div class="card-body">
-                      <p class="mb-4">Number of Clients</p>
-                      <p class="fs-30 mb-2">47033</p>
-                      <p>0.22% (30 days)</p>
+                      <p class="mb-4">August Task</p>
+                      <!-- <p class="fs-30 mb-2">47033</p> -->
+                      <p class="text-danger">Not Completed</p>
                     </div>
                   </div>
                 </div>
