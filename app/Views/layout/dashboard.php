2bf9ae272e6ff23b213eaa57ffc1b814e23d14ce
nicocagui <101541722+nicocagui@users.noreply.github.com>
2023-05-04 11:24:04 +0800

    Update dashboard.php

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -308,7 +308,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
               <i class="icon-layout menu-icon"></i>
-              <span class="menu-title">UI Elements</span>
+              <span class="menu-title">Task Accomplishments</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="ui-basic">
@@ -322,7 +322,7 @@
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
               <i class="icon-columns menu-icon"></i>
-              <span class="menu-title">Form elements</span>
+              <span class="menu-title">Date of Submission</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="form-elements">
