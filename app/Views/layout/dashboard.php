6d6de3f9adff790a9a0609dcbf0f556c9b8d3c3c
Erjhon Baldoza <erjhonbaldoza@gmail.com>
2023-05-04 10:55:46 +0800

    initial

--- app/Views/layout/dashboard.php
+++ app/Views/layout/dashboard.php
@@ -6,6 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title><?= (isset($pageTitle)) ?  $pageTitle: 'DILG System'; ?></title>
+  <!-- Base url -->
   <base href="<?=base_url()?>public/">
   <!-- plugins:css -->
   <link rel="stylesheet" href="vendors/feather/feather.css">
