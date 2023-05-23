b3d3ce1cc00c86a7f14ac23dfba4c24ee084700b
nicocagui <101541722+nicocagui@users.noreply.github.com>
2023-05-23 14:59:41 +0800

    LGCDS

--- app/Views/layout/lgcdd.php
+++ app/Views/layout/lgcdd.php
@@ -297,6 +297,7 @@
           <!-- chat tab ends -->
         </div>
       </div>
+
       <!-- partial -->
       <!-- partial:partials/_sidebar.html -->
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
@@ -335,10 +336,126 @@
         </ul>
       </nav>
 
-   
+      <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
-    
+            <div class="col-md-12 grid-margin">
+              <div class="row">
+                <!-- <div class="col-12 col-xl-8 mb-4 mb-xl-0">
+                  <h3 class="font-weight-bold">Welcome C/MLGOO <?php $session = session(); echo $session->get('name');?></h3>
+                  <h6 class="font-weight-normal mb-0">Here is the updated Memorandum! You have <span class="text-primary">3 unread alerts!</span></h6>
+                </div> -->
+                <div class="col-12 col-xl-4">
+                 <div class="justify-content-end d-flex">
+                  <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
+                    <!-- <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button" id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                     <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
+                    </button> -->
+                    <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
+                      <a class="dropdown-item" href="#">January - March</a>
+                      <a class="dropdown-item" href="#">March - June</a>
+                      <a class="dropdown-item" href="#">June - August</a>
+                      <a class="dropdown-item" href="#">August - November</a>
+                    </div> -->
+                  </div>
+                 </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+<!--<div class="main-panel" img src="images/monitor1.png">
+    <div class="content-wrapper">
+    <div class="row">
+     <div class="col-md-12 grid-margin">
+        <div class="row">-->
+         <!--DILG LOGO--> 
+          <div class="row">
+            <div class="col-md-1">
+              <div class="">
+                <div class="card-people mt-auto">
+                  <img src="images/dilg.png">
+                  <div class="weather-info">
+                    <div class="d-flex">
+                      <div>
+                        <!-- <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2> -->
+                      </div>
+                      <!-- <div class="ml-2">
+                        <h4 class="location font-weight-normal"><b>Memorandum</b></h4>
+                        <h6 class="font-weight-normal">Notice!</h6>
+                      </div> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+             <!--LGRRC LOGO--> 
+          <div class="col">
+            <div class="col-md-1 grid-margin stretch-card">
+              <div class="">
+                <div class="card-people mt-auto">
+                  <img src="images/lgrrc.png">
+                  <div class="weather-info">
+                    <div class="d-flex">
+                      <div>
+                        <!-- <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2> -->
+                      </div>
+                      <!-- <div class="ml-2">
+                        <h4 class="location font-weight-normal"><b>Memorandum</b></h4>
+                        <h6 class="font-weight-normal">Notice!</h6>
+                      </div> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- LGCDD Text-->
+            <h1 align="center" style="font-family:arial black;">LGCDD Central Reporting System</h1>
+            <h3 align="center" style="font-family:verdana;">MONITORING AND REPORTING GUIDE FOR PFPs AND C/MLGOOs </h3>
+          </div>
+
+          <div class="row">
+            <div class="col-md-3 grid-margin">
+							<div class="card d-flex align-items-center">
+								<div class="card-body">
+									<div class="d-flex flex-row align-items-center">
+										<!-- <i class="ti-facebook text-facebook icon-md"></i> -->
+										<div class="ms-3">
+                    <a href="https://sites.google.com/dilg.gov.ph/lgmedcrs/ars" class="link-class">LGU Administration and Governance Capacity Building Section (LAGCBS) </a>
+											<!--<a class="">Awards and Recognition Section </a>-->
+											<!-- <p class="mt-2 text-muted card-text">You main list growing</p> -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 grid-margin">
+							<div class="card d-flex align-items-center">
+								<div class="card-body">
+									<div class="d-flex flex-row align-items-center">
+										<!-- <i class="ti-youtube text-youtube icon-md"></i> -->
+										<div class="ms-3">
+                    <a href="https://sites.google.com/dilg.gov.ph/lgmedcrs/lgpbas" class="link-class">Local Economic Development Section (LEDS) </a>
+											<!-- <p class="mt-2 text-muted card-text">You main list growing</p> -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 grid-margin">
+							<div class="card d-flex align-items-center">
+								<div class="card-body">
+									<div class="d-flex flex-row align-items-center">
+										<!-- <i class="ti-twitter-alt text-twitter icon-md"></i> -->
+										<div class="ms-3">
+                    <a href="https://sites.google.com/dilg.gov.ph/lgmedcrs/poarcs" class="link-class">Disaster Resiliency and Environmental Protection Section (DREPS)  </a>
+											<!-- <p class="mt-2 text-muted card-text">You main list growing</p> -->
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+          </div>
         <!-- content-wrapper ends -->
 
       
